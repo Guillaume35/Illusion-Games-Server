9b314b21704dfd64b44b053b887fb8a607c46a7f
Guillaume Modard <guillaumemodard@gmail.com>
2011-07-29 12:40:49 +0200

    Make IGS object oriented + some bugs fixing

--- sources/igs.py
+++ sources/igs.py
@@ -1,14 +1,73 @@
 import socket, threading, igsconfig, re
 
 # initializing
-registered_mods = []
-clients = 0
+REGISTERED_MODS = []
+CLIENTS = {} # connected clients
+
+class Client(threading.Thread):
+	"""Accept and manage connection with a new client"""
+	
+	def __init__(self, client):
+		threading.Thread.__init__(self)
+		self.client = client
+	
+	def run(self):
+		# Dialog with the client
+		
+		global CLIENTS
+		
+		client_name = self.getName()
+		
+		while True:
+			q = self.client.recv(1024)
+			
+			if not q:
+				break
+			
+			# Remove b'' used to encode message
+			# TODO : Maybe there is a better way ?
+			qstr = str(q)
+			qstr = re.sub(r"^b'", "", qstr)
+			qstr = re.sub(r"'$", "", qstr)
+			qstr = re.sub(r'^b"', '', qstr)
+			qstr = re.sub(r'"$', '', qstr)
+		
+			if re.search(r"^igs\.", qstr):
+				# IGS registered modules
+				qstr = re.sub(r"^igs\.", r"igsmods.", qstr)
+			
+				mod = re.sub(r"^igsmods\.(\w+)\.\w+\(.*\)\s*", r"\1", qstr)
+			
+				if mod in REGISTERED_MODS:
+					try:
+						exec ("import igsmods."+mod) # TODO : Create a list at the beginning of the program and use __import__ in stead of exec
+						r = eval(qstr)
+					except:
+						r = 'ER: There where a problem in your command execution. Check the syntax'
+			
+				else:
+					r = 'ER: Module %s could not be found in IGS registered modules list' % mod
+		
+			else:
+				# TODO : Create registered games list
+				r = 'Launching module %s which is a game' % qstr
+			
+		
+			r = bytes(r, 'UTF-8')
+		
+			self.client.send(r)
+		
+		# Closing the connection 
+		self.client.close()
+		del CLIENTS[client_name]
+		print ("Client %s is disconnected from the server" % client_name)
+		# TODO : Show client IP
 
 def update_registered_mods():
 	"""Make a list with all enabled igs mods"""
 	
-	global registered_mods
-	registered_mods = [] # re-initializing registered_mods
+	global REGISTERED_MODS
+	REGISTERED_MODS = [] # re-initializing REGISTERED_MODS
 	
 	f = open('registered-mods', 'r')
 	
@@ -16,91 +75,60 @@ def update_registered_mods():
 		mod = re.sub(r'\s*$', '', line)
 		mod = re.sub(r'^\s*', '', mod)
 		
-		if not re.search(r'^#', mod) and re.search(r'^\w+$', mod) and mod not in registered_mods:
-			registered_mods.append(mod)
+		if not re.search(r'^#', mod) and re.search(r'^\w+$', mod) and mod not in REGISTERED_MODS:
+			REGISTERED_MODS.append(mod)
 	
 	f.close()
 	
-	return registered_mods
-			
-
-def main_loop(sock):
-	"""Accept connections and for each, call handler() in a new thread"""
-
-	while True:
-		client, addr = sock.accept()
-		print ("Client %s is connecting"%addr[0])
-		
-		# Start a new thread for this client
-		threading.Thread(target=handler, args=(client,  addr)).start()
-
-
-def handler(client, addr):
-	"""Execute client query"""
-	
-	global clients
-	clients += 1
-	
-	# Test security filter TODO
-	security = igsconfig.get('SECURITY')
-	access = 1
-
-	while True:
-		q = client.recv(1024)
-		
-		if not q: #If disconnected
-			break
-		
-		qstr = str(q)
-		qstr = re.sub(r"^b'", "", qstr)
-		qstr = re.sub(r"'$", "", qstr)
-		qstr = re.sub(r'^b"', '', qstr)
-		qstr = re.sub(r'"$', '', qstr)
-		
-		if re.search(r"^igs\.", qstr):
-			qstr = re.sub(r"^igs\.", r"igsmods.", qstr)
-			
-			mod = re.sub(r"^igsmods\.(\w+)\.\w+\(.*\)\s*", r"\1", qstr)
-			
-			if mod in registered_mods:
-				#try:
-				exec ("import igsmods."+mod)
-				r = eval(qstr)
-				#except:
-				#	r = 'ER: There where a problem in your command execution. Check the syntax'
-			
-			else:
-				r = 'ER: Module '+mod+' could not be found in IGS registered modules list'
-		
-		else:
-			r = 'Launching module '+qstr+' which is a game'
-		
-		r = bytes(r, 'UTF-8')
-		
-		client.send(r)
-	
-	print ("Client %s is disconnected from the server"%addr[0])
+	return REGISTERED_MODS
 
 
 def start_server(HOST, PORT):
 	"""Initialize and start IGS"""
+	
+	global CLIENTS
+	
+	print ("* START IGS ON %s:%s *" % (HOST, str(PORT)))
 	
 	# read registered modules
 	mods = update_registered_mods()
 	print ('Modules list registered on this server :')
 	print (mods)
 	
-	sock = socket.socket()
-	sock.bind((HOST, PORT))
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	try:
+		sock.bind((HOST, PORT))
+	except socket.error:
+		print ("ER: Connection with server fail")
+		return 0
+	
+	print ('Ready...')
 	sock.listen(5)
-	main_loop(sock)
+	
+	# Waiting for client connection
+	# Clients dictionnary
+	CLIENTS = {}
+	
+	while True:
+		client, addr = sock.accept()
+		
+		# New object with that manage the client
+		client_thread = Client(client)
+		client_thread.start()
+		
+		# Save connection in the dictionnary
+		thread_id = client_thread.getName()
+		CLIENTS[thread_id] = client
+		
+		print ("Client %s is connected (%s:%s)" % (thread_id, addr[0], addr[1]))
+		
+		#client.send (b"connected")
 
 
 if __name__ == "__main__":
 	# Start IGS
 	
 	HOST, PORT = igsconfig.get('HOST'), igsconfig.get('PORT')
-	
-	print ("* START IGS ON "+HOST+":"+PORT+" *")
 	
 	start_server(HOST,int(PORT))
